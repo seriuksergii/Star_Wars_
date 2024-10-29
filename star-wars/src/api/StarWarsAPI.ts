87f11fd5d8f9e1b02d3cdd8bc47ad80341618881
Sergii Serdiuk <serdiuk-sergii@bustour.ua>
2024-10-29 18:38:17 +0200

    add code

--- star-wars/src/api/StarWarsAPI.ts
+++ star-wars/src/api/StarWarsAPI.ts
@@ -1,17 +1,22 @@
+// Function to get hero data from API
 export const getHeroes = async (page: number) => {
+  // Execute a request to receive data from the API with a page parameter
   const response = await fetch(
     `https://sw-api.starnavi.io/people/?page=${page}`
   );
+  // Check if the answers are successful
   if (!response.ok) {
-    throw new Error('Unable to load hero data');
+    throw new Error('Unable to load hero data'); // Throw an error if the request fails
   }
   const data = await response.json();
+  // Return the results and the total number of heroes
   return {
     results: data.results,
     total: data.count,
   };
 };
 
+// Function to get movie titles by their identifiers
 export const fetchFilmTitles = async (filmIds: string[]) => {
   const titles = await Promise.all(
     filmIds.map(async (id) => {
@@ -23,6 +28,7 @@ export const fetchFilmTitles = async (filmIds: string[]) => {
   return titles;
 };
 
+// Function to get the names of views by their identifiers
 export const fetchSpeciesNames = async (speciesIds: string[]) => {
   const names = await Promise.all(
     speciesIds.map(async (id) => {
@@ -34,6 +40,7 @@ export const fetchSpeciesNames = async (speciesIds: string[]) => {
   return names;
 };
 
+// Function to get the names of the starships by their identifiers
 export const fetchStarshipNames = async (starshipIds: string[]) => {
   const names = await Promise.all(
     starshipIds.map(async (id) => {
@@ -47,6 +54,7 @@ export const fetchStarshipNames = async (starshipIds: string[]) => {
   return names;
 };
 
+// Function to get the names of the planets by their identifiers
 export const fetchPlanetName = async (homeworldIds: string[]) => {
   const names = await Promise.all(
     homeworldIds.map(async (id) => {
@@ -61,6 +69,7 @@ export const fetchPlanetName = async (homeworldIds: string[]) => {
   return names;
 };
 
+// Function to get the names of the vehicles by their identifiers
 export const fetchVehicleNames = async (vehicleIds: string[]) => {
   const names = await Promise.all(
     vehicleIds.map(async (id) => {
